ba9c1f295f34fa8ab86a983e1f450bed235e2c44
jiteshshankhpal <jshankhpal41@gmail.com>
2026-04-09 23:52:51 +0530

    created the sidebar

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const hamEl = document.querySelector('.ham-link');
 const navSec = document.querySelector('.nav-section')
+const mainSec= document.querySelector('.main-section')
+const cancelBtn = document.querySelector('.x-icon')
 
 hamEl.addEventListener('click', (e) => {
     e.preventDefault(); 
     navSec.classList.add('active');
+    mainSec.classList.add('main-blur')
 });
+
+cancelBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    navSec.classList.remove('active')
+    mainSec.classList.remove('main-blur')
+})
+
